d4abea280525d5b53fb9feeb858a99f9224cfcc8
aurebidart <aure.bidart@gmail.com>
2025-09-17 15:50:33 -0300

    Fixing

--- client/python/projectairsim/docs/conf.py
+++ client/python/projectairsim/docs/conf.py
@@ -1,25 +1,37 @@
 # Configuration file for the Sphinx documentation builder.
 
-# Add projectairsim src/ folder to the Python path for sphinx to find it
 import os
 import sys
 
 import sphinx_rtd_theme
 sys.path.insert(0, os.path.abspath("../src"))
 
-project = 'Project AirSim'
-copyright = '2022, Microsoft'
-author = 'Microsoft'
+project = "Project AirSim"
+copyright = "2022, Microsoft"
+author = "Microsoft"
 
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon']
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.autosummary",
+]
 
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+autosummary_generate = True  # build autosummary pages
+autodoc_typehints = "description"
+autodoc_default_options = {
+    "members": True,
+    "undoc-members": True,
+    "show-inheritance": True,
+}
 
+# Si tu paquete importa libs pesadas / no disponibles en CI, mockealas:
+autodoc_mock_imports = [
+    "numpy", "cv2", "pynng", "airsim", "open3d", "torch", "ultralytics",
+    # agregá acá cualquier otra dependencia que se importe a nivel de módulo
+]
 
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
-# -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-
-html_theme = 'sphinx_rtd_theme'
+html_theme = "sphinx_rtd_theme"
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
